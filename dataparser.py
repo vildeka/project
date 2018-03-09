e0fbe172026f58e54ed07660429671ab5a3c9165
vildeka <vilde.kaldhusdal@gmail.com>
2018-03-09 10:33:48 +0100

    changing windowsize

--- dataparser.py
+++ dataparser.py
@@ -20,25 +20,25 @@ def parse_fasta(filename):
 
 #================input vectors words/features==============
 
-def inputvector_X(sequence, window=7):
+def inputvector_X(sequence, window=15):
     padding = window//2
-    
-    
+
+
     # dictionary of aa:
     vals = np.identity(20, dtype=int)
     keys = list("ACDEFGHIKLMNPQRSTVWY")
     aa_dict = dict(zip(keys, vals.T))
     aa_dict['0'] = np.zeros(20, dtype=int)
-        
-    
+
+
     #final input:
     features = []
-    #list of words:   
+    #list of words:
     word_seq = []
-    
+
     for i in range(len(sequence)):
         if i > padding and i < len(sequence) - padding - 1:
-            #-1 because you want second to last element 
+            #-1 because you want second to last element
             word_seq.append(sequence[i-padding:i+padding+1])
             #+1 is to get the last element as well [icluded:notincluded]
         elif i <= padding:
@@ -53,33 +53,33 @@ def inputvector_X(sequence, window=7):
             this_word = sequence[i-1:] #[43-1:]= ASC, [44-1]= SC
             zeros_needed = window - len(this_word)
             word_seq.append(this_word+'0' * zeros_needed)
-        
-    for word in word_seq:    
-        this_word = list(map(lambda n: aa_dict[n], word))    
+
+    for word in word_seq:
+        this_word = list(map(lambda n: aa_dict[n], word))
         features.append(np.concatenate(this_word))
     return np.array(features)
-    
+
 def train_inputvector_X(parse_dict):
     parse_dict = parse_fasta('../project/datasets/datamini.txt')
-    X_vector = [] 
-    for sequence1, topology in parse_dict.values(): 
-        feature = inputvector_X(sequence1, window=7)
+    X_vector = []
+    for sequence1, topology in parse_dict.values():
+        feature = inputvector_X(sequence1, window=15)
         X_vector.extend(feature)
     return np.array(X_vector)
-   
-#================input vector topology==============    
-    
-def inputvector_y(parse_dict): 
+
+#================input vector topology==============
+
+def inputvector_y(parse_dict):
     # dictionary of topology:
     topo = {'H':1, 'S':2, 'C':3}
-    
+
     #final input:
     y_vector = []
-    for sequence, topology in parse_dict.values(): 
-        for i in range(len(topology)): 
+    for sequence, topology in parse_dict.values():
+        for i in range(len(topology)):
            y_vector.append(topo[topology[i]])
-    
-    return np.array(y_vector) 
+
+    return np.array(y_vector)
 #=========================One-hot encoding==============
 # useless shit:
 '''from numpy import argmax
@@ -120,9 +120,9 @@ if __name__ == '__main__':
 
     #result_X = inputvector_X(sequence1, window=3))
     #print (result_X)
-    
+
     result_train_X = train_inputvector_X(parse_fasta('../project/datasets/data.txt'))
     #print (result_train_X)
-    
+
     result_y = inputvector_y(parse_fasta('../project/datasets/data.txt'))
     #print (result_y)
--- train_test.py
+++ train_test.py
@@ -4,14 +4,14 @@ import pickle
 
 from dataparser import parse_fasta, train_inputvector_X, inputvector_y
 
-X = train_inputvector_X(parse_fasta('../project/datasets/data.txt'))
-y = inputvector_y(parse_fasta('../project/datasets/data.txt'))
+#X = train_inputvector_X(parse_fasta('../project/datasets/data.txt'))
+#y = inputvector_y(parse_fasta('../project/datasets/data.txt'))
 
-#X = train_inputvector_X(parse_fasta('../project/datasets/datamini.txt'))
-#y = inputvector_y(parse_fasta('../project/datasets/datamini.txt'))
+X = train_inputvector_X(parse_fasta('../project/datasets/datamini.txt'))
+y = inputvector_y(parse_fasta('../project/datasets/datamini.txt'))
 
 print(X.shape)
-#create model and give other than dtype: 
+#create model and give other than dtype:
 model = svm.SVC(decision_function_shape='ovo')
 #training data:
 model.fit(X, y)
@@ -19,16 +19,16 @@ accuracy = model.score(X, y)
 print(accuracy)
 
 #using pickle to save model to disk:
-filename = '../project/models/whole_dataset_model.sav'
+filename = '../project/models/mini_win15_model.sav'
 pickle.dump(model, open(filename, 'wb'))
 
 
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     result_train_X = train_inputvector_X(parse_fasta('../project/datasets/data.txt'))
     #print (result_train_X)
-    
+
     result_y = inputvector_y(parse_fasta('../project/datasets/data.txt'))
     #print (result_y)
 
@@ -43,4 +43,3 @@ if __name__ == '__main__':
 #http://scikit-learn.org/stable/modules/svm.html
 #http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
 #https://towardsdatascience.com/train-test-split-and-cross-validation-in-python-80b61beca4b6
-
