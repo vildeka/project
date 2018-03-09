0e40b5c388c4fc864fb261151b5ebe6d28315727
vildeka <vilde.kaldhusdal@gmail.com>
2018-03-09 14:22:15 +0100

    windowsize

--- FASTA_to_inputvector.py
+++ FASTA_to_inputvector.py
@@ -10,28 +10,28 @@ def parse_fasta(filename):
                 key = line[1:-1]
             else:
                 A = line.strip("\n")
-                seq_dict[key] = A    
+                seq_dict[key] = A
     return(seq_dict)
-    
 
-def inputvector_X(sequence, window=7): #identical to def inputvector_X(sequence, window=3):
+
+def inputvector_X(sequence, window=15): #identical to def inputvector_X(sequence, window=3):
     padding = window//2
-    
+
     # dictionary of aa:
     vals = np.identity(20, dtype=int)
     keys = list("ACDEFGHIKLMNPQRSTVWY")
     aa_dict = dict(zip(keys, vals.T))
     aa_dict['0'] = np.zeros(20, dtype=int)
-        
-    
+
+
     #final input:
     features = []
-    #list of words:   
+    #list of words:
     word_seq = []
-    
+
     for i in range(len(sequence)):
         if i > padding and i < len(sequence) - padding - 1:
-            #-1 because you want second to last element 
+            #-1 because you want second to last element
             word_seq.append(sequence[i-padding:i+padding+1])
             #+1 is to get the last element as well [icluded:notincluded]
         elif i <= padding:
@@ -46,18 +46,18 @@ def inputvector_X(sequence, window=7): #identical to def inputvector_X(sequence,
             this_word = sequence[i-1:] #[43-1:]= ASC, [44-1]= SC
             zeros_needed = window - len(this_word)
             word_seq.append(this_word+'0' * zeros_needed)
-        
-    for word in word_seq:    
-        this_word = list(map(lambda n: aa_dict[n], word))    
+
+    for word in word_seq:
+        this_word = list(map(lambda n: aa_dict[n], word))
         features.append(np.concatenate(this_word))
     return np.array(features)
 
-    
+
 
 
 if __name__ == '__main__':
-    result_FASTA = parse_fasta('../project/datasets/FASTAfilemini.fasta')
+    result_FASTA = parse_fasta('../project/datasets/FASTAfile.fasta')
     print (result_FASTA)
-    
-    result_FASTA = FASTA_to_inputvectorX(parse_fasta('../project/datasets/FASTAfilemini.fasta'))
+
+    result_FASTA = FASTA_to_inputvectorX(parse_fasta('../project/datasets/FASTAfile.fasta'))
     #print (result_FASTA)
