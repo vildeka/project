e1a301fb3a887fa06259638c2bb1e3096368b467
vildeka <vilde.kaldhusdal@gmail.com>
2018-03-11 17:33:54 +0100

    directory for bash scripts

---
+++ bash/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#cd ../project/datasets/
+#mkdir swiss_database
+makeblastdb -in /home/u2361/project/datasets/PSSM_files/uniprot_sprot.fasta -dbtype prot -out ../project/datasets/PSSM_files/DB_SWISSPROT/swissprot_db
+
+
+
+
+
+#https://github.com/WangXueqing007/KB8024-Bioinformatics-Project
+#https://github.com/carolinasavatier
+
+#echo This bash creates PSSM of FASTA file
+
+#python3 to_singelFASTA.py
+
+#makeblastdb -in /home/u2361/project/datasets/PSSM_files/uniprot_sprot.fasta -dbtype prot -out ../project/datasets/PSSM_files/DB_SWISSPROT/swissprot_db
